8019c64ab1bb2eebbb1bc5da38539ffb3aef3c65
Bert Sultanov <bert.sultanov.contact@gmail.com>
2017-02-11 21:11:04 +0300

    Add tests for Autowire scope

--- tests/unit/AutowireTest.php
+++ tests/unit/AutowireTest.php
@@ -18,11 +18,19 @@ class AutowireTest extends \Codeception\Test\Unit
     }
 
     // tests
-    public function testMa()
+    public function testCreateNew()
     {
         /** @var \vivace\di\type\Scope $scope */
         $scope = $this->tester->newScope([], new \vivace\di\AutoWire());
 
         $this->tester->assertInstanceOf(\vivace\di\tests\Magneto::class, $scope->import(\vivace\di\tests\Magneto::class));
+    }
+
+    public function testExceptionIfNotClass()
+    {
+        $this->tester->expectException(\vivace\di\error\Undefined::class, function () {
+            $scope = $this->tester->newScope([], new \vivace\di\AutoWire());
+            $scope->import('Undefined');
+        });
     }
 }
